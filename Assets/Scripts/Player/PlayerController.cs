d2833baf282ade1a1f6a863036182e762b67c1ce
Reyhaneh-Ghaffari <ghafarireihane601@gmail.com>
2026-07-05 16:52:39 +0330

    InputManager , PlayerController , ShadowController Added By Reyhaneh Ghaffari

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,148 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    // State
+    private Vector2 moveInput;
+    private bool isGrounded;
+    private bool isShadowMode = false;
+    private int jumpCount = 0;
+    private const int MAX_JUMPS = 2;
+
+    // Events
+    public System.Action<bool> OnModeChanged;
+
+    private void Awake()
     {
-        
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+        if (animator == null) animator = GetComponent<Animator>();
+        if (spriteRenderer == null) spriteRenderer = GetComponent<SpriteRenderer>();
+        if (groundCheckPoint == null) 
+        {
+            GameObject go = new GameObject("GroundCheck");
+            go.transform.parent = transform;
+            go.transform.localPosition = new Vector3(0, -0.5f, 0);
+            groundCheckPoint = go.transform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        CheckGrounded();
+        UpdateAnimations();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && jumpCount < MAX_JUMPS)
+        {
+            Jump();
+        }
+    }
+
+    public void OnSwitchMode(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            ToggleShadowMode();
+        }
+    }
+
+    private void Move()
+    {
+        float moveX = moveInput.x * moveSpeed;
+        rb.velocity = new Vector2(moveX, rb.velocity.y);
+
+        // Flip sprite based on movement direction
+        if (moveX > 0.1f)
+            spriteRenderer.flipX = false;
+        else if (moveX < -0.1f)
+            spriteRenderer.flipX = true;
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        jumpCount++;
+        AudioManager.Instance?.PlaySFX("Jump");
+    }
+
+    private void CheckGrounded()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(
+            groundCheckPoint.position, 
+            groundCheckRadius, 
+            groundLayer
+        );
         
+        bool wasGrounded = isGrounded;
+        isGrounded = colliders.Length > 0;
+
+        if (isGrounded && !wasGrounded)
+        {
+            jumpCount = 0;
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        float speed = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", speed);
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetBool("IsShadowMode", isShadowMode);
+    }
+
+    private void ToggleShadowMode()
+    {
+        isShadowMode = !isShadowMode;
+        OnModeChanged?.Invoke(isShadowMode);
+        
+        //// Changing layers for obstacle collisions
+        if (isShadowMode)
+        {
+            gameObject.layer = LayerMask.NameToLayer("Shadow");
+          // Disable collision with light objects
+        }
+        else
+        {
+            gameObject.layer = LayerMask.NameToLayer("Player");
+        }
+        
+        AudioManager.Instance?.PlaySFX("SwitchMode");
+    }
+
+    public bool IsShadowMode() => isShadowMode;
+    public bool IsGrounded() => isGrounded;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheckPoint != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundCheckPoint.position, groundCheckRadius);
+        }
     }
 }
--- Assets/Scripts/Player/ShadowController.cs
+++ Assets/Scripts/Player/ShadowController.cs
@@ -1,18 +1,134 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShadowController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Shadow Settings")]
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float teleportDistance = 3f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject shadowVisual;
+    [SerializeField] private GameObject lightVisual;
+
+    private Vector2 moveInput;
+    private int jumpCount = 0;
+    private const int MAX_JUMPS = 3; // Shadow can dash three times.
+    private bool isGrounded;
+    private bool isAttached = true;
+
+    private Transform playerTransform;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // The shadow is initially on the character.
+        AttachToPlayer();
     }
+
+    private void Update()
+    {
+        CheckGrounded();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isAttached)
+        {
+            Move();
+        }
+    }
+
+    public void DetachFromPlayer()
+    {
+        isAttached = false;
+        transform.parent = null;
+        rb.isKinematic = false;
+
+        // Position the shadow exactly where the character is.
+        transform.position = playerTransform.position;
+
+        // Enable shadow physics
+        rb.bodyType = RigidbodyType2D.Dynamic;
+
+        // Change shadow layer to pass through specific obstacles
+        gameObject.layer = LayerMask.NameToLayer("Shadow");
+
+        // Show shadow and hide object
+        shadowVisual.SetActive(true);
+        lightVisual.SetActive(false);
+    }
+
+    public void AttachToPlayer()
+    {
+        isAttached = true;
+        transform.parent = playerTransform;
+        transform.localPosition = Vector3.zero;
+        rb.isKinematic = true;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+
+        // Hide shadow when attached to the body
+        shadowVisual.SetActive(false);
+        lightVisual.SetActive(true);
+    }
+
+    public void Move()
+    {
+        float moveX = moveInput.x * moveSpeed;
+        rb.velocity = new Vector2(moveX, rb.velocity.y);
+
+        if (moveX > 0.1f) spriteRenderer.flipX = false;
+        else if (moveX < -0.1f) spriteRenderer.flipX = true;
+    }
+
+    public void Jump()
+    {
+        if (jumpCount < MAX_JUMPS)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount++;
+            AudioManager.Instance?.PlaySFX("ShadowJump");
+        }
+    }
+
+    public void Teleport(Vector2 direction)
+    {
+        // Casting a shadow towards the platform
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            direction,
+            teleportDistance,
+            LayerMask.GetMask("Platform")
+        );
+
+        if (hit.collider != null)
+        {
+            transform.position = hit.point + Vector2.up * 0.5f;
+            AudioManager.Instance?.PlaySFX("ShadowTeleport");
+        }
+    }
+
+    private void CheckGrounded()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(
+            transform.position + Vector3.down * 0.5f,
+            0.2f,
+            LayerMask.GetMask("Ground", "Platform")
+        );
+        isGrounded = colliders.Length > 0;
+
+        if (isGrounded) jumpCount = 0;
+    }
+
+    public void SetMoveInput(Vector2 input) => moveInput = input;
+    public bool IsAttached() => isAttached;
+    public bool IsGrounded() => isGrounded;
 }
